78b5cdfce1b268e8e86cee53869f0fe525d5e087
AysuDemirci <aysu.gunerr@gmail.com>
2023-05-19 23:00:10 +0300

    edited the page

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,69 +1,131 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { Col, Container, Input } from "reactstrap";
+import React from "react";
+import { Col, Container, Row } from "reactstrap";
+import { FaCloudSun } from "react-icons/fa";
 
-export default function Weather() {
-  const [city, setCity] = useState("");
-  const [weatherInfo, setWeatherInfo] = useState({});
-  const [isActive, setIsActive] = useState(false);
+export default function Weather(props) {
+  const { weatherInfo, isActive, tempUnit } = props;
 
-  function handleChange(e) {
-    setCity(e.target.value);
-  }
-
-  const handleClick = async () => {
-    const baseUrl = `https://api.openweathermap.org/data/2.5/weather?&appid=a14e7ab880c601d7c8d6ecea90cf71f5&units=metric`;
-
-    await axios
-      .get(baseUrl, { params: { q: city } })
-      .then(async (data) => {
-        setWeatherInfo(data.data);
-      })
-      .catch((err) => console.log("hata sebebi: ", err));
-    setIsActive(true);
-  };
-
+  const date = new Date().toLocaleDateString();
   return (
     <div>
       <Container>
         <br />
-        <div>
-          <div className="form">
-            <center>
-              <Col md="5">
-                <Input
-                  value={city}
-                  className="weather-input"
-                  type="text"
-                  placeholder="Enter City Name"
-                  onChange={handleChange}
-                />
+        <br />
+        <div className="weather-div">
+          <Row>
+            <Col md="9" className="weather-col">
+              {isActive ? (
                 <div>
-                  <button onClick={handleClick}>Get Data of City</button>
-                </div>
-              </Col>
-            </center>
-          </div>
+                  <Row>
+                    <Col style={{ padding: "10px 0px 0px 30px" }}>
+                      <p style={{ fontSize: "35px" }}>
+                        {weatherInfo.name},{weatherInfo.sys.country}
+                      </p>
+                    </Col>
+                    <Col style={{ marginTop: "20px", marginLeft: "65%" }}>
+                      <p style={{ fontSize: "20px", fontWeight: "600" }}>
+                        {date}
+                      </p>
+                    </Col>
+                  </Row>
 
-          {isActive ? (
-            <div>
-              <br />
-              <center>
-                <p style={{ fontSize: "40px" }}>
-                  {weatherInfo.name},{weatherInfo.sys.country}
-                </p>
-                <p>Temperature : {weatherInfo.main.temp.toFixed(0)}°C</p>
-                <p>
-                  Felt Temperature : {weatherInfo.main.feels_like.toFixed(0)}°C
-                </p>
-                <p>
-                  Weather Condition :
-                  {weatherInfo.weather[0].description[0].toUpperCase() +
-                    weatherInfo.weather[0].description.substring(1)}
-                </p>
-              </center>
-            </div>
-          ) : null}
+                  <Col style={{ padding: "10px 0px 0px 20px" }}>
+                    <Row>
+                      <ul
+                        style={{
+                          display: "flex",
+                          listStyle: "none",
+                          gap: "20px",
+                        }}
+                      >
+                        <li>
+                          <div>
+                            {weatherInfo.weather[0].description ===
+                            "broken clouds" ? (
+                              <p>{<FaCloudSun className="weather-icons" />}</p>
+                            ) : null}
+                          </div>
+                        </li>
+                        <li>
+                          <div>
+                            {tempUnit === "metric" ? (
+                              <p className="temp-style">
+                                {weatherInfo.main.temp.toFixed(0)}°C
+                              </p>
+                            ) : (
+                              <p className="temp-style">
+                                {weatherInfo.main.temp.toFixed(0)}°F
+                              </p>
+                            )}
+                          </div>
+                        </li>
+                        <li style={{ marginLeft: "65%" }}>
+                          <div>
+                            <ul
+                              style={{
+                                listStyle: "none",
+                                display: "flex",
+                                gap: "20%",
+                              }}
+                            >
+                              <li>
+                                <p>
+                                  Day
+                                  {tempUnit === "metric" ? (
+                                    <p className="weather-day-night">
+                                      {weatherInfo.main.temp_max.toFixed(0)}°C
+                                    </p>
+                                  ) : (
+                                    <p className="weather-day-night">
+                                      {weatherInfo.main.temp_max.toFixed(0)}°F
+                                    </p>
+                                  )}
+                                </p>
+                              </li>
+                              <li style={{ marginTop: "0px" }}>
+                                <p>
+                                  Night
+                                  {tempUnit === "metric" ? (
+                                    <p className="weather-day-night">
+                                      {weatherInfo.main.temp_min.toFixed(0)}°C
+                                    </p>
+                                  ) : (
+                                    <p className="weather-day-night">
+                                      {weatherInfo.main.temp_min.toFixed(0)}°F
+                                    </p>
+                                  )}
+                                </p>
+                              </li>
+                            </ul>
+                          </div>
+                        </li>
+                      </ul>
+
+                      <div style={{ marginTop: "-25px" }}>
+                        Real Feel
+                        {tempUnit === "metric" ? (
+                          <p className="weather-day-night">
+                            {weatherInfo.main.feels_like.toFixed(0)}°C
+                          </p>
+                        ) : (
+                          <p className="weather-day-night">
+                            {weatherInfo.main.feels_like.toFixed(0)}°F
+                          </p>
+                        )}
+                      </div>
+                      <div style={{ fontSize: "22px" }}>
+                        {weatherInfo.weather[0].description[0].toUpperCase() +
+                          weatherInfo.weather[0].description.substring(1)}
+                      </div>
+                    </Row>
+                  </Col>
+
+                  <br />
+                </div>
+              ) : null}
+            </Col>
+          </Row>
+          <br />
         </div>
       </Container>
     </div>
